15802d5e0d1b487aec803f0b29137359bbc651d7
k22shreyas <shreyaskaranam22@gmail.com>
2022-06-01 11:40:37 +0530

    applied tesseract with python itself

--- captcha_solver.py
+++ captcha_solver.py
@@ -2,6 +2,7 @@ import time
 import os
 import pyautogui
 import cv2 as cv
+import pytesseract
 
 time.sleep(4)
 myScreenshot = pyautogui.screenshot(region=(995,390, 170, 80)) #region=(vertical pos, horizontal pos, vertical ratio, horizontal ratio)
@@ -17,5 +18,11 @@ os.chdir('D:\web_scrap\captcha')
 cv.imwrite("thresh_img.jpg", thresh)
 
 time.sleep(2)
-os.system('"wsl tesseract thresh_img.jpg result"') #tesseract is ocr function for image to text
+#os.system('"wsl tesseract thresh_img.jpg result"') #tesseract is ocr function for image to text
+img2 = cv.imread('thresh_img.jpg',0)
 
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe' 
+#depends on your tesseract installation path
+custom_config = r'--oem 3 --psm 6'
+captcha = pytesseract.image_to_string(img2, config=custom_config)
+print(captcha)
