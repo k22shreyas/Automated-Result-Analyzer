4dc656b3664dfeb32c7cc2033371eba707bbb539
k22shreyas <shreyaskaranam22@gmail.com>
2022-06-01 11:51:23 +0530

    comments added

--- captcha/ccc.py
+++
@@ -1,6 +0,0 @@
-import pyautogui
-import os
-
-myScreenshot = pyautogui.screenshot(region=(0,0, 300, 400))
-myScreenshot.save(r'D:\screenshot.png')
-os.system('wsl ~ -e sh -c " tesseract \\wsl.localhost\Ubuntu\home\thor\sum\captcha\\image1.png stdout"')
--- captcha_solver.py
+++ captcha_solver.py
@@ -3,10 +3,11 @@ import os
 import pyautogui
 import cv2 as cv
 import pytesseract
+#pip install all the above libraries
 
 time.sleep(4)
 myScreenshot = pyautogui.screenshot(region=(995,390, 170, 80)) #region=(vertical pos, horizontal pos, vertical ratio, horizontal ratio)
-myScreenshot.save(r'D:\web_scrap\screenshot.png')
+myScreenshot.save(r'D:\web_scrap\screenshot.png') #change according to your dir
 
 os.chdir('D:\web_scrap')
 img = cv.imread('screenshot.png',0)
@@ -20,9 +21,10 @@ cv.imwrite("thresh_img.jpg", thresh)
 time.sleep(2)
 #os.system('"wsl tesseract thresh_img.jpg result"') #tesseract is ocr function for image to text
 img2 = cv.imread('thresh_img.jpg',0)
-
+#install tesseract from https://github.com/UB-Mannheim/tesseract/wiki choose 64-bit 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe' 
 #depends on your tesseract installation path
 custom_config = r'--oem 3 --psm 6'
 captcha = pytesseract.image_to_string(img2, config=custom_config)
 print(captcha)
+#captcha contains converted text
