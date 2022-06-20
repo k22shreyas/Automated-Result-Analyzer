ea80e012bf4ca26cda3c6d40b9a6de90ec91de6c
k22shreyas <shreyaskaranam22@gmail.com>
2022-06-20 20:06:15 +0530

    Errors are handled properly with valid logging

--- vtu_result.py
+++ vtu_result.py
@@ -1,7 +1,6 @@
-from openpyxl import load_workbook
 from selenium import webdriver
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoAlertPresentException
+from selenium.common.exceptions import NoSuchElementException
 import time
 import csv
 import os
@@ -9,133 +8,142 @@ import pyautogui
 import pandas as pd
 import cv2 as cv
 import pytesseract
-# install all these as pip install filename, and pip install opencv-python.
 
-f = open("mark.csv", "w")
+f = open("D:\web_scrap\\result\marks.csv", "w")
 f.truncate()
 f.close()
 
 option = webdriver.ChromeOptions()
 option.add_argument("-incognito")
-option.add_argument("start-maximized")
 option.add_experimental_option("excludeSwitches", ['enable-automation'])
 option.add_experimental_option("detach",True)
 
-#add your chrome driver installation path
 browser = webdriver.Chrome(executable_path=r'C:\Program Files (x86)\chromedriver.exe', options=option)
+browser.set_window_size(600, 800)
+def fillLoginpage(usn, subject_codes):
 
-def fillLoginpage(usn, ite):
-
-    browser.get("https://results.vtu.ac.in/FMEcbcs22/resultpage.php")
-
-    #getting hold of usn and captcha input fields.
+    browser.get("https://results.vtu.ac.in/FMEcbcs22/index.php")
 
     testbox = browser.find_element_by_xpath("/html/body/div[2]/div[1]/div[2]/div/div[2]/form/div/div[2]/div[1]/div/input")
     captchabox = browser.find_element_by_xpath("/html/body/div[2]/div[1]/div[2]/div/div[2]/form/div/div[2]/div[2]/div[1]/input")
 
-    #start with the image capta recognition procedure
-    time.sleep(2)
-    myScreenshot = pyautogui.screenshot(region=(970, 405, 170, 80)) #region=(horizontal pos, vertical pos, vertical ratio, horizontal ratio)
+    time.sleep(1)
+    myScreenshot = pyautogui.screenshot(region=(45, 415, 170, 80)) #region=(horizontal pos, vertical pos, vertical ratio, horizontal ratio)
     myScreenshot.save(r'D:\web_scrap\captcha\captcha_img.png') #change according to your dir.
 
     os.chdir('D:\web_scrap')
     img = cv.imread('captcha\captcha_img.png',0)
     ret,thresh = cv.threshold(img,103,150,cv.THRESH_TOZERO_INV)
-    cv.imshow('Binary Threshold', thresh)
-    # Using cv2.imwrite() method
     os.chdir('D:\web_scrap\captcha')
-    cv.imwrite("thresh_img.png", thresh)
+    cv.imwrite("threshold_img.png", thresh)
 
     time.sleep(1)
-    #os.system('"wsl tesseract thresh_img.jpg result"') #tesseract is ocr function for image to text
-    img2 = cv.imread('thresh_img.png',0)
+    img2 = cv.imread('threshold_img.png',0)
     #install tesseract from https://github.com/UB-Mannheim/tesseract/wiki choose 64-bit 
     pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe' 
-    #depends on your tesseract installation path
     custom_config = r'--oem 3 --psm 6'
     captcha = pytesseract.image_to_string(img2, config=custom_config)
-    
     captcha.replace(" ", "").strip()
 
     print("Printing solved Captcha " +captcha)
-    #Exception and Error handling
+
     if(len(captcha)-1 != 6 ):
         return -1
     time.sleep(1)
+
     try:
         testbox.send_keys(usn)
         captchabox.send_keys(captcha) 
     except:
         return -1
+
     try:
-        print(browser.current_url)
-    except:
-        return -1
-    time.sleep(2)
+        obj = browser.switch_to.alert
+        msg=obj.text
+        obj.accept()
+        print(msg)
+        if(msg == "Invalid captcha code !!!"):
+            return -1
+        if(msg == "University Seat Number is not available or Invalid..!"):
+            return 1
+    except NoAlertPresentException: 
 
-    marks_list = []
-    marks_list.append(usn)
-    subject_codes = []
-    
-    for cols in range(3, sheet.max_column+1):
-        cell_subject = sheet.cell(row=1,column=cols)
-        subject = cell_subject.value
-        subject_codes.append(subject)
-    print(subject_codes)
-    
-    try:
-        for sub_code in subject_codes:
-            #name = browser.find_element_by_xpath("//*[@id='dataPrint']/div[2]/div/div/div[2]/div[1]/div/div/div[1]/div/table/tbody/tr[2]/td[2]/text()").text
-            internal_marks = browser.find_element_by_xpath("//*[@id='dataPrint']//*[contains(text(),'"+sub_code+"')]//following::div[2]").text
-            external_marks = browser.find_element_by_xpath("//*[@id='dataPrint']//*[contains(text(),'"+sub_code+"')]//following::div[3]").text
-            total_marks = browser.find_element_by_xpath("//*[@id='dataPrint']//*[contains(text(),'"+sub_code+"')]//following::div[4]").text
-            remarks = browser.find_element_by_xpath("//*[@id='dataPrint']//*[contains(text(),'"+sub_code+"')]//following::div[5]").text
+        marks_list = []
+        marks_list.append(usn)
+        
+        try:
+            sub_code = 0
+            while sub_code < len(subject_codes):
+                internal_marks = browser.find_element_by_xpath("//*[@id='dataPrint']//*[contains(text(),'"+subject_codes[sub_code]+"')]//following::div[2]").text
+                external_marks = browser.find_element_by_xpath("//*[@id='dataPrint']//*[contains(text(),'"+subject_codes[sub_code]+"')]//following::div[3]").text
+                total_marks = browser.find_element_by_xpath("//*[@id='dataPrint']//*[contains(text(),'"+subject_codes[sub_code]+"')]//following::div[4]").text
+                remarks = browser.find_element_by_xpath("//*[@id='dataPrint']//*[contains(text(),'"+subject_codes[sub_code]+"')]//following::div[5]").text
 
-            marks_list.append(internal_marks)
-            marks_list.append(external_marks)
-            marks_list.append(total_marks)
-            marks_list.append(remarks)
-            print(marks_list)
-    except:
-        return 1
-        #Error handling
-    print(marks_list)
-    os.chdir('D:\web_scrap\\result')
-    #fields = ["USN", "18ME751", "18CS71", "18CS72","18CS744","18CS734","18CSL76","18CSP77"]
-    with open('marks.csv', 'a') as f:
-    # using csv.writer method from CSV package
-        write = csv.writer(f)
-        write.writerow(marks_list)
-    time.sleep(2)
+                marks_list.append(internal_marks)
+                marks_list.append(external_marks)
+                marks_list.append(total_marks)
+                marks_list.append(remarks)
+                sub_code +=1
+        
+        except NoSuchElementException:
+            print("Invalid (USN, Subject Code) combination")
+            with open('D:\web_scrap\\result\marks.csv', 'a',) as f:
+                write = csv.writer(f)
+                marks_list.append("NA")
+                write.writerow(marks_list)
+            csv_read = pd.read_csv('D:\web_scrap\\result\marks.csv')
+            csv2excel = pd.ExcelWriter('D:\web_scrap\\result\student_marks.xlsx')
+            csv_read.to_excel(csv2excel)
+            csv2excel.save()
+            return 1 
+        
+        print(marks_list)
+        with open('D:\web_scrap\\result\marks.csv', 'a') as f:
+            write = csv.writer(f)
+            write.writerow(marks_list)
+        csv_read = pd.read_csv('D:\web_scrap\\result\marks.csv')
+        csv2excel = pd.ExcelWriter('D:\web_scrap\\result\student_marks.xlsx')
+        csv_read.to_excel(csv2excel)
+        csv2excel.save()
 
-    csv_read = pd.read_csv('marks.csv')
-    csv2excel = pd.ExcelWriter('student_marks.xlsx')
-    csv_read.to_excel(csv2excel, index=True, header=False)
-    csv2excel.save()
-
-filepath=r"D:\web_scrap\result\student_usn_subject.xlsx"    #excel path
-wb=load_workbook(filepath)                                                         # load into wb
-sheet=wb.active                                                                    # active workbook
-#store and pass current usn to function
 def main():
-    ite=3
+    ite=0
+    student_usn = []
+    file = open('D:\web_scrap\\result\student_usn.csv')
+    csvreader = csv.reader(file)
+    for usns in csvreader:
+        student_usn.append(usns[0])
+    
+    headerList = ['USN']
+    subject_codes = []
+    file = open('D:\web_scrap\\result\codes.csv')
+    csvreader = csv.reader(file)
+    for row in csvreader:
+        subject_codes.append(row[0])
+        headerList.append('Internals')
+        headerList.append('Externals')
+        headerList.append('Total')
+        headerList.append('Remarks')
+    print(subject_codes)
+
+    with open('D:\web_scrap\\result\marks.csv', 'a') as file:
+        dw = csv.DictWriter(file, delimiter=',', fieldnames=headerList)
+        dw.writeheader()
+
     print("START")
-    while ite <= sheet.max_row:
-        cell_obj = sheet.cell(row=ite, column=1)
-        usn = cell_obj.value
-        x = fillLoginpage(usn, ite)
-        print("IN MAIN FUNC") #for testing
-        print(ite) #for testing
+    while ite < len(student_usn):
+        usn = student_usn[ite]
+        print(usn)
+        x = fillLoginpage(usn, subject_codes)
         if(x == 1):
-            print(x)
+            #Iterate with next usn as error handling
             ite = ite+1
             continue
         elif(x == -1):
-            print("IN ekif block")
-            print(ite)
+            #iterate with same usn
             continue
+        #Iterate with next usn
         ite = ite+1
-
 
 if __name__ == "__main__":
     main()
